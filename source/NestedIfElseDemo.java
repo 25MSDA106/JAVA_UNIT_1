523d7ebe0a196f2f24305a0147a15849709334fb
25MSDA106 <prasanna.m25@gcu.edu.in>
2025-11-27 11:07:02 +0530

    NestedIfElseDemo.java

--- source/NestedIfElseDemo.java
+++ source/NestedIfElseDemo.java
@@ -1,13 +1,14 @@
 class NestedIfElseDemo {
-public static void main(String[] args) {
-int num = -5;
-if (num >= 0) {
-if (num == 0)
-System.out.println("Number is Zero");
-else
-System.out.println("Number is Positive");
-} else {
-System.out.println("Number is Negative");
-}
-}
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num >= 0) {
+            if (num == 0)
+                System.out.println("Number is Zero");
+            else
+                System.out.println("Number is Positive");
+        } else {
+            System.out.println("Number is Negative");
+        }
+    }
 }
